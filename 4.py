67169834652347ea1f44770719f9c0cde18c5692
sanyamwork98 <sanyamworks98@gmail.com>
2025-05-31 18:04:02 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,18 @@
+def print_rangoli(size):
+    import string 
+    alphabet = string.ascii_lowercase
+    lines = []
+    for i in range(size):
+        left = alphabet[size-1:i:-1] 
+        right = alphabet[i:size]      
+        row = '-'.join(left + right)
+        lines.append(row.center(4*size - 3, '-'))
+        
+    full_rangoli = lines[::-1][:-1] + lines
+    print('\n'.join(full_rangoli))
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
